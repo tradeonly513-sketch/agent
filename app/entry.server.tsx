009ec1c22ed9b16b269ac8e3ec15d3af1389cc0f
Shirisha <124602484+shirisha22@users.noreply.github.com>
2025-04-17 22:29:12 -0700

    Revert "Fix base URL"
    This reverts commit 53ab1405216d62b19c12fc6c0f515e514ff4831e.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -16,7 +16,7 @@ export default async function handleRequest(
 ) {
   // await initializeModelList({});
 
-  const response = await authenticate(request, _loadContext);
+  const response = await authenticate(request);
 
   if (!response.authenticated && response.response instanceof Response) {
     const clonedResponse = response.response.clone();
