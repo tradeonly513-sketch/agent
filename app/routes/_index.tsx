b9e7210960fb578b20234de0446f96c6e4b73592
Syed Ismail Zabiulla <syedismailart@gmail.com>
2025-07-29 00:52:53 +0530

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { Header } from '~/components/header/Header';
 import BackgroundRays from '~/components/ui/BackgroundRays';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'zawr.ai' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'zawr.ai' }, { name: 'description', content: 'Talk with zawr, an AI assistant from Zawr Industries' }];
 };
 
 export const loader = () => json({});
