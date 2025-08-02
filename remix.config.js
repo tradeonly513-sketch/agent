9855937bbf9565050f4f01acaeb28278b887c381
ErnestoPavlicek <perriiitoo@gmail.com>
2025-08-02 18:54:43 +0000

    build(remix): use CJS server bundle

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+/** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  // Make sure Remix builds for a Node/Express runtime (not Cloudflare/Edge)
   serverPlatform: "node",
-
-  // Keep ESM since your package.json is "type": "module"
-  serverModuleFormat: "esm",
+  serverModuleFormat: "cjs",
 };
+
