2f9444b3409ca4cde394a0f33a952990b2e1a4de
ErnestoPavlicek <perriiitoo@gmail.com>
2025-08-02 16:48:46 +0000

    build(server): force node platform

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  serverModuleFormat: "cjs",
+  // Make sure Remix builds for a Node/Express runtime (not Cloudflare/Edge)
   serverPlatform: "node",
+
+  // Keep ESM since your package.json is "type": "module"
+  serverModuleFormat: "esm",
 };
