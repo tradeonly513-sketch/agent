bb309d4428db4b6d1600182ab0b270918fae1a0a
ErnestoPavlicek <perriiitoo@gmail.com>
2025-08-02 15:34:02 +0000

    build(server): node platform + cjs

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.all(
-  "*",
-  createRequestHandler({
-    build: await import("./build/server/index.js"),
+app.all("*", async (req, res, next) => {
+  const mod = await import("./build/server/index.js");
+  const build = mod.default ?? mod; // works for both ESM and CJS
+  return createRequestHandler({
+    build,
     mode: process.env.NODE_ENV,
-  })
-);
+  })(req, res, next);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
