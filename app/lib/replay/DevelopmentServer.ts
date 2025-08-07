9b1500b436146c8480c7f6c750c77b6cd5dc2f80
Brian Hackett <bhackett1024@gmail.com>
2025-08-07 10:07:37 -0700

    Remove fetch() call causing CORS errors (#228)

--- app/lib/replay/DevelopmentServer.ts
+++ app/lib/replay/DevelopmentServer.ts
@@ -18,9 +18,4 @@ export const updateDevelopmentServer = debounce((repositoryId: string | undefine
   workbenchStore.showWorkbench.set(repositoryURL !== undefined);
   workbenchStore.repositoryId.set(repositoryId);
   workbenchStore.previewURL.set(repositoryURL);
-
-  // Prefetch the development server so it will be ready earlier.
-  if (repositoryURL) {
-    fetch(repositoryURL);
-  }
 }, 500);
