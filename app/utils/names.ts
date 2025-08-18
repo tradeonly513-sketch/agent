5bd717702e6adc93cea348ba097e70b6bd811e14
Brian Hackett <bhackett1024@gmail.com>
2025-08-18 13:09:31 -0700

    Tweak how pascal case names are handled (#270)

--- app/utils/names.ts
+++ app/utils/names.ts
@@ -2,9 +2,9 @@
 
 // A PascalCase name is converted to a more readable "Pascal Case" name for display.
 export function formatPascalCaseName(name: string) {
-  // Insert spaces before every capital letter, except the first one and when
-  // the previous letter is also capital.
-  return name.replace(/([A-Z])/g, (match, letter, index) => {
+  // Insert spaces before every capital letter / number, except the first one and when
+  // the previous letter is also capital / number.
+  return name.replace(/([A-Z0-9])/g, (match, letter, index) => {
     // Don't add space before the first letter
     if (index === 0) {
       return letter;
